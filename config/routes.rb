8eddfe883a318b3947a9ebf31c55cb42b5b0e7f1
Valery <skelet2394@gmail.com>
2018-03-19 17:08:42 +0300

    Showing projects+todos without layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  get 'todo/index'
+
+  get 'todo/update'
+
+  get 'todo/create'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'welcome/index'
 
-  root 'welcome#index'
+  root 'todo#index'
 end
